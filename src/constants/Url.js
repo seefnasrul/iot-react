94e099d1357a47bf9e809501d10877847832ec9e
Seef Bambang <seefnasrul@gmail.com>
2020-10-29 02:05:03 +0700

    change base url :

--- src/constants/Url.js
+++ src/constants/Url.js
@@ -1,6 +1,6 @@
 export default {
     // API:'http://10.80.21.2/adzkia_api/index.php',
     // BASE:'http://10.80.21.2/adzkia_api'
-    API:'http://127.0.0.1:8000/api',
-    BASE:'http://localhost:3000'
+    API:'https://adzkia.org/api',
+    BASE:'https://adzkia.org'
   };
